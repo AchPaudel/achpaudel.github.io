04b9be97d9175b8214443ea6e221a164be048494
achyut <a953p488@ku.edu>
2022-05-25 17:13:21 -0500

    Homepage set up.
    Content needs to be added next.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,6 @@
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
 		});
