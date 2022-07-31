38c368d4b2d277b7aeb18353b2d13a54702b6e18
AchPaudel <achpaudel@ku.edu>
2022-07-31 12:04:14 -0500

    animationUpdates

--- main.js
+++ main.js
@@ -17,15 +17,22 @@ var tlProjectsBase = new TimelineMax();
 var tlProjects = new TimelineMax();
 var tlProjectsRow1 = new TimelineMax();
 
+var tlSkills = new TimelineMax();
+
 tlSecondScroll
-.fromTo('.intro', 2, {y:"200%"}, {y:"0%"});
+.fromTo(".trigger1", 4, {x:"-200%"}, {x:"0%"})
+.fromTo(".intro", 4, {x:"-200%"}, {x:"0%"}, "-=5")
 
 tlThirdScroll
-.fromTo('.aboutme', 1, {y:"200%"}, {y:"0%"});
+.fromTo(".trigger2", 4, {y:"200%"}, {y:"0%"})
+.fromTo(".aboutme", 4, {y:"200%"}, {y:"0%"}, "-=5")
 
 tlFourthScroll
-.fromTo(".skillsBox", 1, {y:"200%"}, {y:"0%"})
-.fromTo(".certifications", 1, {y:"200%"}, {y:"0%"}, "-=1");
+.fromTo(".trigger3", 12, {x:"200%"}, {x:"0%"}, "12")
+.fromTo(".skillsHeader", 8, {x: "200%"}, {x: "0%"}, ">")
+.fromTo([".skillLabel", ".skills", ".skillsBox .container"], 8, {x: "200%"}, {x: "0%"})
+.fromTo(".certsHeader", 8, {x: "200%"}, {x: "0%"})
+.fromTo(".certs", 8, {x: "1000%"}, {x: "0%"});
 
 tlProjectsBase
 .fromTo(".projectsHeader", 5, {y: "-50%"}, {y: "200%"});
@@ -33,40 +40,41 @@ tlProjectsBase
 tlProjects
 .fromTo(".trigger6", 2, {y: "200%"}, {y: "0%"});
 
+
 tlProjectsRow1
 .fromTo(".trigger7", 1, {scale: 0.25, x: "200%", y:"-50%", rotation:"180", }, {ease: Bounce.easeOut, scale:0.80 , x: "0%", y:"0%", rotation:"360"}, "-=2")
 .fromTo(".trigger8", 1, {scale: 0.25, x: "-200%", y:"-50%", rotation:"-180"}, {ease: Bounce.easeOut, scale: 0.80, x: "0%", y:"0%", rotation:"-360"}, "-=2")
-.fromTo(".trigger9", 2,  {scale: 0.25, x: "200%", y:"-50%", rotation:"180"}, {ease: Bounce.easeOut, scale: 0.70, x: "0%", y:"0%", rotation:"360"}, "1.5")
-.fromTo(".trigger10", 2,  {scale: 0.25, x: "-200%", y:"-50%", rotation:"-180"}, {ease: Bounce.easeOut, scale: 0.70, x: "0%", y:"0%", rotation:"-360"}, "1.5")
-.fromTo(".trigger11", 2,  {scale: 0.25, x: "200%", y:"-150%", rotation:"180"}, {ease: Bounce.easeOut, scale: 0.80, x: "0%", y:"0%", rotation:"360"}, "4")
-.fromTo(".trigger12", 2, {scale: 0.25, x: "-200%", y:"-150%", rotation:"-180"}, {ease: Bounce.easeOut, scale: 0.80, x: "0%", y:"0%", rotation:"-360"}, "4");
+.fromTo(".trigger9", 1.5,  {scale: 0.25, x: "200%", y:"-50%", rotation:"180"}, {scale: 0.70, x: "0%", y:"0%", rotation:"360"}, "0.75")
+.fromTo(".trigger10", 1.5,  {scale: 0.25, x: "-200%", y:"-50%", rotation:"-180"}, {scale: 0.70, x: "0%", y:"0%", rotation:"-360"}, "0.75")
+.fromTo(".trigger11", 2,  {scale: 0.25, x: "200%", y:"-150%"}, {scale: 0.80, x: "0%", y:"0%"}, "2.25")
+.fromTo(".trigger12", 2, {scale: 0.25, x: "-200%", y:"-150%"}, {scale: 0.80, x: "0%", y:"0%"}, "2.25");
 
 
-var scene1 = new ScrollMagic.Scene({
-    triggerElement: ".trigger1",
+var scene2 = new ScrollMagic.Scene({
+    triggerElement: ".SM",
     triggerHook: "onEnter",
     duration: "100%"
 })
 .setTween(tlSecondScroll)
 .addTo(controller);
 
-var scene2 = new ScrollMagic.Scene({
-    triggerElement: ".trigger2",
-    triggerHook: "onEnter",
+var scene3 = new ScrollMagic.Scene({
+    triggerElement: ".trigger1",
+    triggerHook: "onCenter",
     duration: "100%"
 })
 .setTween(tlThirdScroll)
 .addTo(controller);
 
-var scene3 = new ScrollMagic.Scene({
-    triggerElement: ".trigger3",
-    triggerHook: "onEnter",
+var scene4 = new ScrollMagic.Scene({
+    triggerElement: ".trigger2",
+    triggerHook: "onCenter",
     duration: "100%"
 })
 .setTween(tlFourthScroll)
 .addTo(controller);
 
-var scene4 = new ScrollMagic.Scene({
+var scene5 = new ScrollMagic.Scene({
     triggerElement: ".projectsBase",
     triggerHook: "onEnter",
     duration: "100%"
@@ -74,7 +82,7 @@ var scene4 = new ScrollMagic.Scene({
 .setTween(tlProjectsBase)
 .addTo(controller);
 
-var scene5 = new ScrollMagic.Scene({
+var scene6 = new ScrollMagic.Scene({
     triggerElement: ".projectsBase",
     triggerHook: "onEnter",
     duration: "75%"
@@ -83,7 +91,7 @@ var scene5 = new ScrollMagic.Scene({
 .setPin(".trigger6")
 .addTo(controller);
 
-var scene6 = new ScrollMagic.Scene({
+var scene7 = new ScrollMagic.Scene({
     triggerElement: ".trigger6",
     triggerHook: "onLeave",
     duration: "300%"
